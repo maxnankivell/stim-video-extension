9c4bfd235a9ff429646c8ce00167495b4189b816
maxnankivell <maxnankivell1@gmail.com>
2023-02-06 11:13:24 +1300

    added presets

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # stim-video-extension
+
+A chrome extension I wrote to play video within the chrome dev tools
+with the intent of playing minecraft parkour or subway surfer type
+videos while watching educational content to help with focus.
--- options.js
+++ options.js
@@ -1,5 +1,11 @@
 const input = document.getElementById('url-input');
 const saveButton = document.getElementById('save-button');
+const minecraftParkourButton = document.getElementById('minecraft-parkour');
+const subwaySurferButton = document.getElementById('subway-surfer');
+
+chrome.storage.sync.get(['url'], (res) => {
+    input.value = res.url ?? ``;
+})
 
 saveButton.addEventListener('click', () => {
     const url = input.value;
@@ -8,6 +14,16 @@ saveButton.addEventListener('click', () => {
     })
 })
 
-chrome.storage.sync.get(['url'], (res) => {
-    input.value = res.name ?? ``;
+minecraftParkourButton.addEventListener('click', () => {
+    const url = `https://www.youtube.com/watch?v=a5S-YhfjZdI`;
+    chrome.storage.sync.set({
+        url
+    })
+})
+
+subwaySurferButton.addEventListener('click', () => {
+    const url = `https://www.youtube.com/watch?v=LN_OJIsheyk`;
+    chrome.storage.sync.set({
+        url
+    })
 })
